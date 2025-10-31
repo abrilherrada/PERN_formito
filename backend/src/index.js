13938ce0c9fda3d2f3baddfde438d320313ca35a
Abril Herrada <abrilherrada@gmail.com>
2025-10-31 16:15:01 -0300

    Bootstrap Formito project

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Formito API' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
