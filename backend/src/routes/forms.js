f379e0c6ad01ac14d080ab08bcced0fb0b854a6b
Abril Herrada <abrilherrada@gmail.com>
2025-11-14 16:17:16 -0300

    Wire submission endpoints into forms router

--- backend/src/routes/forms.js
+++ backend/src/routes/forms.js
@@ -7,25 +7,43 @@ import {
   formIdSchema,
 } from '../utils/validation/forms.js';
 import {
+  submissionParamsSchema,
+  submissionDetailParamsSchema,
+  submissionBodySchema
+} from '../utils/validation/submissions.js';
+import {
   createForm,
   findFormById,
   findFormsByUserId,
   updateForm,
   deleteForm,
 } from '../controllers/forms.js';
+import {
+  createSubmission,
+  findSubmissionsByFormId,
+  findSubmissionById,
+} from '../controllers/submissions.js';
 
 const router = express.Router();
 
+// Public
+router.post('/:formId', validateRequest(submissionParamsSchema, 'params'), validateRequest(submissionBodySchema), createSubmission);
+
+// Private
 router.use(verifyToken);
 
 router.post('/', validateRequest(createFormSchema), createForm);
 
 router.get('/', findFormsByUserId);
 
-router.get('/:id', validateRequest(formIdSchema, 'params'), findFormById);
+router.get('/:formId', validateRequest(formIdSchema, 'params'), findFormById);
 
-router.patch('/:id', validateRequest(formIdSchema, 'params'), validateRequest(updateFormSchema), updateForm);
+router.patch('/:formId', validateRequest(formIdSchema, 'params'), validateRequest(updateFormSchema), updateForm);
 
-router.delete('/:id', validateRequest(formIdSchema, 'params'), deleteForm);
+router.delete('/:formId', validateRequest(formIdSchema, 'params'), deleteForm);
+
+router.get('/:formId/submissions', validateRequest(submissionParamsSchema, 'params'), findSubmissionsByFormId);
+
+router.get('/:formId/submissions/:submissionId', validateRequest(submissionDetailParamsSchema, 'params'), findSubmissionById);
 
 export default router;
