803886d323ffd5e426d77206c273ec8409096867
Abril Herrada <abrilherrada@gmail.com>
2025-12-03 19:56:05 -0300

    Add cookie-parser middleware and wire it into the express app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.js';
 import userEmailsRouter from './routes/userEmails.js';
 import formsRouter from './routes/forms.js';
@@ -13,6 +14,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
