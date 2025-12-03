de0470ed5b42f5de1229f71930fc7e8b9bd04922
Abril Herrada <abrilherrada@gmail.com>
2025-12-03 20:00:51 -0300

    Add repositories for session token workflows

---
+++ backend/src/repositories/sessionToken.js
@@ -0,0 +1,85 @@
+import { prisma } from '../../prisma/client.js';
+
+const buildInclude = ({ includeUser = false, includeRelations = false } = {}) => {
+  const include = {};
+
+  if (includeUser) {
+    include.user = true;
+  }
+
+  if (includeRelations) {
+    include.rotatedFrom = true;
+    include.replacedByToken = true;
+  }
+
+  return Object.keys(include).length ? include : undefined;
+};
+
+export const createSessionTokenRepository = async ({
+  userId,
+  tokenHash,
+  expiresAt,
+  userAgent = null,
+  ipAddress = null,
+  rotatedFromId = null,
+}) => {
+  return await prisma.sessionToken.create({
+    data: {
+      userId,
+      tokenHash,
+      expiresAt,
+      userAgent,
+      ipAddress,
+      ...(rotatedFromId
+        ? {
+            rotatedFrom: {
+              connect: { id: rotatedFromId },
+            },
+          }
+        : {}),
+    },
+  });
+};
+
+export const findSessionTokenByHashRepository = async (
+  tokenHash,
+  { includeUser = false, includeRelations = false } = {}
+) => {
+  const include = buildInclude({ includeUser, includeRelations });
+
+  return await prisma.sessionToken.findUnique({
+    where: { tokenHash },
+    ...(include ? { include } : {}),
+  });
+};
+
+export const updateSessionTokenRepository = async (sessionTokenId, data) => {
+  return await prisma.sessionToken.update({
+    where: { id: sessionTokenId },
+    data,
+  });
+};
+
+export const revokeSessionTokensForUserRepository = async (
+  userId,
+  { excludeIds = [], revokedAt = new Date(), revokedReason } = {}
+) => {
+  const where = {
+    userId,
+    revokedAt: null,
+    ...(excludeIds.length ? { id: { notIn: excludeIds } } : {}),
+  };
+
+  const data = {
+    revokedAt,
+  };
+
+  if (revokedReason !== undefined) {
+    data.revokedReason = revokedReason;
+  }
+
+  return await prisma.sessionToken.updateMany({
+    where,
+    data,
+  });
+};
